6dffaf7fc8fd610cac2975f78880e606d33e0b32
Angel_21 <74176323+abeja21@users.noreply.github.com>
2021-11-23 17:55:51 +0100

    combustion created

--- Carrera.js
+++ Carrera.js
@@ -1,41 +1,46 @@
 class Carrera {
-    circuito= new Circuitos('marsella',3,['r','r','r','c','c','c','c','r','r','r','r','c','r','r'])
-    coches =  [new Ferrari(200,4,2,true), new Tesla(230,3,1,true)];
+    circuito = new Circuitos('marsella', 3, ['r', 'r', 'r', 'c', 'c', 'c', 'c', 'r', 'r', 'r', 'r', 'c', 'r', 'r'])
+    coches = [new Ferrari(200, 4, 2, true), new Tesla(230, 3, 1, true)];
     resultados = [];
     instante = 0;
-    corriendo=true;
+    corriendo = true;
 
-    start(){
-        
+    start() {
+
         while (this.corriendo) {
             this.instante++;
-        console.log(this.resultados.length)
-            this.coches.map((coche)=>{
-                if(coche.ingame==true){
-                console.log(`${coche.name }->${coche.dr}; v:${coche.vel}`)
-                if(coche.dr<this.circuito.distancia ){
-                this.juego(coche,this.circuito.pista[parseInt(coche.dr/100)]);
-                }else{
-                    coche.ingame=false;
-                    this.resultados.push({instante:this.instante,coche})
-                    if(this.resultados.length == this.coches.length){
-                        this.corriendo=false;
+            console.log(this.resultados.length)
+            this.coches.map((coche) => {
+                if (coche.ingame == true) {
+                    if (coche.fuel >= 30) {
+                        console.log(`${coche.name}->${coche.dr}; v:${coche.vel}`)
+                        if (coche.dr < this.circuito.distancia) {
+                            this.juego(coche, this.circuito.pista[parseInt(coche.dr / 100)]);
+                        } else {
+                            coche.ingame = false;
+                            this.resultados.push({ instante: this.instante, coche })
+                            if (this.resultados.length == this.coches.length) {
+                                this.corriendo = false;
+                            }
+                        }
+                    } else {
+                        coche.refuel();
+
                     }
                 }
-            }
-            
-            
+
+
             })
-            
+
         }
         console.log(this.resultados)
     }
 
-    juego(coche,posicion) {
-        switch(posicion) {
+    juego(coche, posicion) {
+        switch (posicion) {
             case 'r':
-               coche.acelerar();
-               
+                coche.acelerar();
+
                 break;
             case 'c':
                 coche.frenar();
--- Coches.js
+++ Coches.js
@@ -18,22 +18,26 @@ class Coches{
             this.vel = this.vel + this.acc;
             this.vel >= this.vmax ? this.vel = this.vmax : this.vel;
             this.dr = this.dr + this.vel;
-            this.fuel-=this.vel/100;
+            this.combustion();
     }
 
     frenar(){
         if(this.vel >= 20){
         this.vel=this.vel-this.deacc;
         this.dr = this.dr + this.vel;
-        this.fuel-=this.vel/100;
+        this.combustion();
         }else{
         this.acelerar();
         }
 
     }
+
+    combustion(){
+        this.fuel-=this.vel/100;
+    }
     
     refuel(){
-        this.fuel+=15;
+        this.fuel+=10;
         this.fuel>this.tank ? this.gas=this.tank : this.fuel;
     }
 
