cdda3e5c8f15af01c85b82b39ee0f5aa88229e01
angel <angel@angel.com>
2021-11-23 12:00:37 +0100

    game don't work why :C

--- Carrera.js
+++ Carrera.js
@@ -1,28 +1,31 @@
 class Carrera {
     circuito= new Circuitos('marsella',3,['r','r','r','c','c','r','c','r','r'])
-    coches =  [new Coches(1,200,4,true), new Coches(2,230, 3,true)];
+    coches =  [new Coches(1,200,4), new Coches(2,230, 3)];
     resultados = [];
     instante = 0;
-
+    corriendo=true;
 
     start(){
         
-        while (this.coches.length !== this.resultados.length) {
+        while (this.corriendo) {
         console.log(this.resultados.length)
             this.coches.map((coche)=>{
                 console.log(coche.jugabilidad)
-                console.log(`${coche.id }->${coche.dr}`)
+                console.log(`${coche.id }->${coche.dr}; v:${coche.vel}`)
                 if(coche.dr<this.circuito.distancia && coche.jugabilidad==true ){
                 this.juego(coche,this.circuito.pista[parseInt(coche.dr/100)]);
                 }else{
                     coche.jugabilidad=false;
                     this.resultados.push({instante:this.instante,coche})
+                    if(this.resultados.length == this.coches.length){
+                        this.corriendo=false;
+                    }
                 }
                
             })
             
         }
-
+        
     }
 
     juego(coche,posicion) {
@@ -33,7 +36,7 @@ class Carrera {
                 break;
             case 'c':
                 coche.frenar();
-                
+                console.log("hey")
                 break;
         }
     }
