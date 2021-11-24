f1ac49b25ae2996131f3bdaa540b6e72d44ba056
Sergio <slorentemontoro@gmail.com>
2021-11-24 09:09:52 +0100

    sergio lo arregla(como siempre)

--- Ferrari.js
+++
@@ -1,9 +0,0 @@
-class Ferrari extends Coches{
-    name="ferrari";
-    tank=150;
-    constructor(vmax,acc,deacc,ingame){
-        super(vmax,acc,deacc,ingame);
-        
-    }
-}
-const ferrari = new Coches()
--- Tesla.js
+++
@@ -1,7 +0,0 @@
-class Tesla extends Coches{
-    name="tesla";
-    tank=200;
-    constructor(vmax,acc,deacc,ingame){
-        super(vmax,acc,deacc,ingame);
-    }
-}
